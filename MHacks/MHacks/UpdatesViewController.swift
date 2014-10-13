ffa6dd7ea3fec8cedc91a34cc87f8249a5ef5b5c
Ben Oztalay <boztalay@gmail.com>
2014-10-13 17:03:19 -0400

    Started on the Updates screen

---
+++ MHacks/MHacks/UpdatesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UpdatesViewController.swift
+//  MHacks
+//
+//  Created by Ben Oztalay on 10/13/14.
+//  Copyright (c) 2014 MHacks. All rights reserved.
+//
+
+import UIKit
+
+class UpdatesViewController: UIViewController {
+    
+}
