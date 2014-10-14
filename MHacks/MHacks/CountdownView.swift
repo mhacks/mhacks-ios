815c60f341aa55329c17f1b4d63c3daf17e7a5ab
Ben Oztalay <boztalay@gmail.com>
2014-10-13 20:34:17 -0400

    Got Parse in the CountdownView

--- MHacks/MHacks/CountdownView.swift
+++ MHacks/MHacks/CountdownView.swift
@@ -12,5 +12,23 @@ class CountdownView: UIView {
 
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        let countdownItemsQuery = PFQuery(className: "CountdownItem")
+        countdownItemsQuery.findObjectsInBackgroundWithBlock { objects, error in
+            if let error = error {
+                println(error)
+                return
+            }
+            
+            var nextCountdownItem: NSDate?
+            
+            if let objects = objects {
+                for object in objects {
+                    println(object["title"])
+                }
+            }
+        }
+    }
 
 }
