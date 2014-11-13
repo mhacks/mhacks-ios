5ed5796bf5bbc6852bc548619a4a15439e3911c0
Ben Oztalay <boztalay@gmail.com>
2014-11-12 22:01:20 -0500

    Split the sponsors into sections, working on section headers

---
+++ MHacks/MHacks/SponsorTierSeparator.swift
@@ -0,0 +1,15 @@
+//
+//  SponsorTierSeparatorCollectionReusableView.swift
+//  MHacks
+//
+//  Created by Ben Oztalay on 11/12/14.
+//  Copyright (c) 2014 MHacks. All rights reserved.
+//
+
+import UIKit
+
+class SponsorTierSeparator: UICollectionReusableView {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
