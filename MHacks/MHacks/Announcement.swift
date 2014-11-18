5a027b5ca44f2a5119ec1f20578fbee118e982f8
Russell Ladd <russell.ladd@grl5.com>
2014-11-18 14:02:20 -0500

    Added base announcement classes

---
+++ MHacks/MHacks/Announcement.swift
@@ -0,0 +1,14 @@
+//
+//  Announcement.swift
+//  MHacks
+//
+//  Created by Russell Ladd on 11/18/14.
+//  Copyright (c) 2014 MHacks. All rights reserved.
+//
+
+import Foundation
+
+struct Announcement {
+    
+    
+}
---
+++ MHacks/MHacks/AnnouncementsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AnnouncementsViewController.swift
+//  MHacks
+//
+//  Created by Russell Ladd on 11/18/14.
+//  Copyright (c) 2014 MHacks. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementsViewController: UITableViewController {
+    
+    
+}
