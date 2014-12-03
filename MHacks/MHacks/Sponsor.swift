0b80ed74f37e7ee83f5b6f6e98814aa104bcae99
Russell Ladd <russell.ladd@grl5.com>
2014-12-03 17:18:09 -0500

    Fleshed out sponsor struct Updated sponsor tier header

--- MHacks/MHacks/Sponsor.swift
+++ MHacks/MHacks/Sponsor.swift
@@ -10,5 +10,79 @@ import Foundation
 
 struct Sponsor {
     
+    let name: String
+    let description: String
+    let website: String
+    let tier: Tier
+    let location: Location
     
+    private let logoFile: PFFile
+    
+    private(set) var logo: UIImage?
+    
+    mutating func fetchLogo() {
+        logoFile.getDataInBackgroundWithBlock { data, error in
+            
+            if data != nil {
+                self.logo = UIImage(data: data) ?? UIImage()
+            }
+        }
+    }
+    
+    mutating func evictLogo() {
+        logo = nil
+    }
+    
+    struct Tier {
+        
+        let name: String
+        let level: Int
+    }
+}
+
+extension Sponsor: Fetchable {
+    
+    init?(object: PFObject) {
+        
+        let name = object["name"] as? String
+        let description = object["description"] as? String
+        let website = object["website"] as? String
+        let tierObject = object["tier"] as? PFObject
+        let locationObject = object["location"] as? PFObject
+        let logoFile = object["logo"] as? PFFile
+        
+        if name == nil || description == nil || website == nil || tierObject == nil || locationObject == nil || logoFile == nil {
+            return nil
+        }
+        
+        let tier = Tier(object: tierObject!)
+        let location = Location(object: locationObject!)
+        
+        if (tier == nil || location == nil) {
+            return nil
+        }
+        
+        self.name = name!
+        self.description = description!
+        self.website = website!
+        self.tier = tier!
+        self.location = location!
+        self.logoFile = logoFile!
+    }
+}
+
+extension Sponsor.Tier: Fetchable {
+    
+    init?(object: PFObject) {
+        
+        let name = object["name"] as? String
+        let level = (object["level"] as? NSNumber)?.integerValue
+        
+        if (name == nil || level == nil) {
+            return nil
+        }
+        
+        self.name = name!
+        self.level = level!
+    }
 }
