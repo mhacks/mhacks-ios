55455c3f2e08bdb5bc80f51620b6468a61350799
Russell Ladd <russell.ladd@grl5.com>
2014-12-02 21:49:17 -0500

    Added sponsor

---
+++ MHacks/MHacks/Sponsor.swift
@@ -0,0 +1,14 @@
+//
+//  Sponsor.swift
+//  MHacks
+//
+//  Created by Russell Ladd on 11/24/14.
+//  Copyright (c) 2014 MHacks. All rights reserved.
+//
+
+import Foundation
+
+struct Sponsor {
+    
+    
+}
