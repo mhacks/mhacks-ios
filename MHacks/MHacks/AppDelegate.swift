cd96b8fe60557292697f0caf4be859fe4dc11323
Russell Ladd <russell.ladd@grl5.com>
2014-12-10 19:11:04 -0500

    Started working on notifications

--- MHacks/MHacks/AppDelegate.swift
+++ MHacks/MHacks/AppDelegate.swift
@@ -19,6 +19,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
+        let settings = UIUserNotificationSettings(forTypes: .Sound | .Alert, categories: nil)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        
+        return true
+    }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        
+        
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
