ac201899d64676a41481f5f8f033e593fc9875b0
Russell Ladd <russell.ladd@grl5.com>
2016-05-04 02:58:01 -0400

    Cleaned up launch experience, opens to countdown

--- MHacks/AppDelegate.swift
+++ MHacks/AppDelegate.swift
@@ -13,10 +13,20 @@ import GoogleMaps
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-
+	
+	var tabBarController: UITabBarController!
+	
+	// MARK: Application life cycle
+	
+	func application(application: UIApplication, willFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+		
+		tabBarController = window!.rootViewController as! UITabBarController
+		
+		return true
+	}
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-		// Override point for customization after application launch.
+		
 		GMSServices.provideAPIKey("AIzaSyDZwjHS79q4iV2_ZWWYvcNDRYzhdYKGoFQ")
 		application.registerForRemoteNotifications()
 		let settings = UIUserNotificationSettings(forTypes: [.Sound, .Alert], categories: nil)
@@ -24,16 +34,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		NSNotificationCenter.defaultCenter().listenFor(.Failure, observer: self, selector: #selector(AppDelegate.error(_:)))
 		
-		
-		if let notif = launchOptions?[UIApplicationLaunchOptionsLocalNotificationKey] as? UILocalNotification {
-			handleLaunch(notif.userInfo?["id"] as? String)
-		}
-		if let _ = launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] as? [String: AnyObject] {
-			handleLaunch(nil) // Must be an announcement
+		switch launchOptions?[UIApplicationLaunchOptionsLocalNotificationKey] {
+			
+		case let notification as UILocalNotification:
+			if let eventID = notification.userInfo?["id"] as? String {
+				showEventWithID(eventID)
+			}
+			
+		case _ as [String: AnyObject]:
+			// Show announcements
+			tabBarController.selectedIndex = 3
+			
+		default:
+			// Show countdown
+			tabBarController.selectedIndex = 2
 		}
 		
 		return true
 	}
+	
 	func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
 		
 		if let _ = defaults.objectForKey(remoteNotificationTokenKey) as? String
@@ -47,28 +66,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
 		NSNotificationCenter.defaultCenter().post(.Failure, object: error.localizedDescription)
 	}
+	
 	func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
 		APIManager.sharedManager.updateAnnouncements()
 		completionHandler(.NewData)
-	}
-	
-	func applicationWillResignActive(application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 	}
 
 	func applicationDidEnterBackground(application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 		APIManager.sharedManager.archive()
-	}
-
-	func applicationWillEnterForeground(application: UIApplication) {
-		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-	}
-
-	func applicationDidBecomeActive(application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 	}
 
 	func applicationWillTerminate(application: UIApplication) {
@@ -82,40 +89,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			return false
 		}
 		let eventID = url.absoluteString.stringByReplacingOccurrencesOfString("mhacks://", withString: "")
-		handleLaunch(eventID.isEmpty ? nil : eventID)
+		showEventWithID(eventID)
 		return true
 	}
 	
-	func handleLaunch(eventID: String?) {
-		guard let tabBar = window?.rootViewController as? UITabBarController, let eventController = tabBar.viewControllers?[0] as? ScheduleCalendarViewController
-		else {
+	// MARK: Show event
+	
+	func showEventWithID(eventID: String) {
+		
+		guard let eventController = tabBarController.viewControllers?[0] as? ScheduleCalendarViewController else {
 			assertionFailure("You changed the structure of the app but not thoroughly enough! We depend on a particular structure to be able to handle push notifications. You must consider restructuring this as part of your change")
 			return
 		}
-		assert(tabBar.viewControllers?[3] is AnnouncementsViewController, "You changed the structure of the app but not thoroughly enough! We depend on a particular structure to be able to handle push notifications. You must consider restructuring this as part of your change")
-		if let eventID = eventID {
-			tabBar.selectedIndex = 0
-			let displayEvent = { () -> Bool in
-				if let (day, index) = APIManager.sharedManager.eventsOrganizer.findDayAndIndexForEventWithID(eventID) {
-					eventController.showDetailsForEvent(APIManager.sharedManager.eventsOrganizer.eventAtIndex(index, inDay: day))
-					return true
-				}
-				return false
+		
+		assert(tabBarController.viewControllers?[3] is AnnouncementsViewController, "You changed the structure of the app but not thoroughly enough! We depend on a particular structure to be able to handle push notifications. You must consider restructuring this as part of your change")
+		
+		tabBarController.selectedIndex = 0
+		
+		let displayEvent = { () -> Bool in
+			if let (day, index) = APIManager.sharedManager.eventsOrganizer.findDayAndIndexForEventWithID(eventID) {
+				eventController.showDetailsForEvent(APIManager.sharedManager.eventsOrganizer.eventAtIndex(index, inDay: day))
+				return true
 			}
-			guard !displayEvent()
-			else { return }
-			APIManager.sharedManager.updateEvents { succeeded in
-				guard displayEvent()
-				else {
-					NSNotificationCenter.defaultCenter().post(.Failure, object: "Could not find the associated event")
-					return
-				}
-			}
+			return false
 		}
-		else {
-			tabBar.selectedIndex = 3
+		
+		guard !displayEvent() else {
+			return
+		}
+		
+		APIManager.sharedManager.updateEvents { succeeded in
+			guard displayEvent() else {
+				NSNotificationCenter.defaultCenter().post(.Failure, object: "Could not find the associated event")
+				return
+			}
 		}
 	}
+	
+	// MARK: Status window
 	
 	var statusWindow : UIWindow?
 	var label : UILabel?
