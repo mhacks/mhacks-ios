ce01ad85d2faaff5160bdc06782b322cbd0687b8
Russell Ladd <russell.ladd@grl5.com>
2016-09-11 18:17:05 -0400

    Started working on Ticket UI

---
+++ MHacks/UserViewController.swift
@@ -0,0 +1,103 @@
+//
+//  UserViewController.swift
+//  MHacks
+//
+//  Created by Russell Ladd on 9/11/16.
+//  Copyright © 2016 MHacks. All rights reserved.
+//
+
+import UIKit
+import PassKit
+
+final class UserViewController: UIViewController {
+    
+    let signInView = UIStackView()
+    let ticketView = UIStackView()
+    
+    let signInTitleLabel = UILabel()
+    let signInDescriptionLabel = UILabel()
+    let signInButton = UIButton(type: .system)
+    
+    let nameTitleLabel = UILabel()
+    let nameLabel = UILabel()
+    
+    let emailTitleLabel = UILabel()
+    let emailLabel = UILabel()
+    
+    let addPassButton = PKAddPassButton(style: .black)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup sign in view
+        
+        signInTitleLabel.text = NSLocalizedString("My Ticket", comment: "Sign in title")
+        signInTitleLabel.textColor = UIColor.gray
+        signInTitleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        signInDescriptionLabel.textColor = UIColor.lightGray
+        signInDescriptionLabel.text = NSLocalizedString("Tickets are only available to registered hackers.", comment: "Sign in description")
+        signInDescriptionLabel.numberOfLines = 0
+        signInDescriptionLabel.textAlignment = .center
+        
+        signInButton.setTitle(NSLocalizedString("Sign In", comment: "Sign in button title"), for: .normal)
+        
+        signInView.translatesAutoresizingMaskIntoConstraints = false
+        signInView.axis = .vertical
+        signInView.spacing = 8.0
+        signInView.alignment = .center
+        
+        signInView.addArrangedSubview(signInTitleLabel)
+        signInView.addArrangedSubview(signInDescriptionLabel)
+        signInView.addArrangedSubview(signInButton)
+        
+        // Setup ticket view
+        
+        ticketView.translatesAutoresizingMaskIntoConstraints = false
+        ticketView.axis = .vertical
+        ticketView.distribution = .equalSpacing
+        
+        ticketView.addArrangedSubview(nameLabel)
+        ticketView.addArrangedSubview(emailLabel)
+        ticketView.addArrangedSubview(addPassButton)
+        
+        // Setup view
+        
+        let contentLayoutGuide = UILayoutGuide()
+        view.addLayoutGuide(contentLayoutGuide)
+        
+        view.addSubview(signInView)
+        view.addSubview(ticketView)
+        
+        NSLayoutConstraint.activate([
+            contentLayoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15.0),
+            contentLayoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15.0),
+            contentLayoutGuide.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 15.0),
+            contentLayoutGuide.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -15.0),
+            signInView.centerXAnchor.constraint(equalTo: contentLayoutGuide.centerXAnchor),
+            signInView.centerYAnchor.constraint(equalTo: contentLayoutGuide.centerYAnchor),
+            signInView.widthAnchor.constraint(equalTo: contentLayoutGuide.widthAnchor, multiplier: 0.75),
+            ticketView.leadingAnchor.constraint(equalTo: contentLayoutGuide.leadingAnchor),
+            ticketView.trailingAnchor.constraint(equalTo: contentLayoutGuide.trailingAnchor),
+            ticketView.topAnchor.constraint(equalTo: contentLayoutGuide.topAnchor),
+            ticketView.bottomAnchor.constraint(equalTo: contentLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func updateViews() {
+        
+        if case .LoggedIn(let user) = APIManager.shared.userState {
+            
+            signInView.isHidden = true
+            ticketView.isHidden = false
+            
+            nameLabel.text = user.name
+            emailLabel.text = user.email
+            
+        } else {
+            
+            signInView.isHidden = false
+            ticketView.isHidden = true
+        }
+    }
+}
